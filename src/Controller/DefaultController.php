a21906ba0816dacef002eb62204ad1353d048d60
Troy Leonard <troyanth@umich.edu>
2017-11-20 19:41:35 -0500

    typecast and sort tracks for theming. add blank functions for user lists

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,13 +10,16 @@ use Drupal\Core\Controller\ControllerBase;
 //use Drupal\Core\Url;
 
 /**
- * Default controller for the weed module.
+ * Default controller for the arborcat module.
  */
 class DefaultController extends ControllerBase {
 
   public function index() {
     return [
-      '#theme' => 'catalog'
+      '#theme' => 'catalog',
+      '#catalog_slider' => [],
+      '#community_slider' => [],
+      '#podcast_slider' => []
     ];
   }
 
@@ -28,6 +31,11 @@ class DefaultController extends ControllerBase {
     $json = $guzzle->get("http://$api_url/record/$bnum")->getBody()->getContents();
     $bib_record = json_decode($json);
 
+    if ($bib_record->tracks) {
+      $bib_record->tracks = (array) $bib_record->tracks;
+      ksort($bib_record->tracks);
+    }
+
     // grab user api key for account actions
     $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
     $user_api_key = $user->field_api_key->value;
@@ -38,5 +46,29 @@ class DefaultController extends ControllerBase {
       '#record' => $bib_record,
       '#api_key' => $user_api_key
     ];
+  }
+
+  public function user_lists() {
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+  }
+
+  public function view_user_list() {
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+  }
+
+  public function create_list() {
+
+  }
+
+  public function add_list_item() {
+
+  }
+
+  public function delete_list() {
+
+  }
+
+  public function delete_list_item() {
+
   }
 }
