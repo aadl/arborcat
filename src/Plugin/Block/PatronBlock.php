bd08edf642a9d5031fe1a4893c10f8e835e61a89
Troy Leonard <troyanth@umich.edu>
2018-01-01 18:26:29 -0500

    update patron summary display

--- src/Plugin/Block/PatronBlock.php
+++ src/Plugin/Block/PatronBlock.php
@@ -27,15 +27,18 @@ class PatronBlock extends BlockBase {
     $guzzle = \Drupal::httpClient();
     $patron = json_decode($guzzle->get("$api_url/patron/$api_key/get")->getBody()->getContents());
     $fines = json_decode($guzzle->get("$api_url/patron/$api_key/fines")->getBody()->getContents());
+    $uid = $user->get('uid')->value;
+    $email = $user->get('mail')->value;
     $payment_link = ($fines->total ? ' (<a href="/fees-payment">pay fees</a>)' : '');
 
     $output = '<h2 id="account-sum" class="no-margin">Account Summary</h2>';
     $output .= "<img id=\"bcode-img\" src=\"$api_url/patron/$api_key/barcode\" alt=\"Image of barcode for scanning at selfchecks\">"; 
     $output .= '<table class="account-summary" class="l-overflow-clear"><tbody>';
-    $output .= "<tr><th scope=\"row\">Library Card Number</th><td>$patron->card</td></tr>";
+    $output .= "<tr><th scope=\"row\">Library Card Number</th><td>$patron->card <a href=\"/user/$uid/edit/barcode\">(edit)</td></tr>";
     $output .= "<tr><th scope=\"row\">Account Balance</th><td>$" . number_format($fines->total, 2) . $payment_link . "</td></tr>";
     $output .= "<tr><th scope=\"row\">Card Expiration Date</th><td>" . date('m-d-Y', strtotime($patron->expires)) . "</td></tr>";
-    $output .= "<tr><th scope=\"row\">Account Email</th><td>$patron->email <a href=\"\">(edit)</a></td></tr>";
+    $output .= "<tr><th scope=\"row\">Account Email</th><td>$email</a></td></tr>";
+    $output .= "<tr><th scope=\"row\">Notifications Sent To</th><td>$patron->email</a></td></tr>";
     $output .= '</tbody></table>';
 
     // $output .= '<a href="" class="button l-overflow-clear" role="button">Add another library card</a>';
