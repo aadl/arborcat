60eb341f4a6cd03a86a9712f28aa035ad5aae0a6
Troy Leonard <troyanth@umich.edu>
2018-03-07 14:23:52 -0500

    fix timestamp since we changed schema

--- arborcat_lists/src/Controller/DefaultController.php
+++ arborcat_lists/src/Controller/DefaultController.php
@@ -196,6 +196,7 @@ class DefaultController extends ControllerBase {
         ->fields([
           'list_id' => $lid,
           'bib' => $bib,
+          'timestamp' => time(),
           'list_order' => $count + 1
         ])
         ->execute();
