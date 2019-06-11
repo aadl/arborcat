becb824bc0d6d9c975e145a871f50861b4857015
Troy Leonard <troyanth@umich.edu>
2019-06-11 11:03:35 -0400

    pass current game term to catalog record

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -120,6 +120,7 @@ class DefaultController extends ControllerBase {
         if (\Drupal::config('summergame.settings')->get('summergame_points_enabled') ||
             $user->hasPermission('play test summergame')) {
           $bib_record->sg_enabled = true;
+          $bib_record->sg_term = \Drupal::config('summergame.settings')->get('summergame_current_game_term');
           $bib_record->gamecodes = (array) $bib_record->gamecodes;
         }
       }
