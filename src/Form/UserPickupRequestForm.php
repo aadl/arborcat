d163417448a7581bff2453f34e342c345c4670f4
Troy Leonard <troyanth@umich.edu>
2020-06-21 13:03:15 -0400

    fix update hold guzzle call

--- src/Form/UserPickupRequestForm.php
+++ src/Form/UserPickupRequestForm.php
@@ -261,7 +261,7 @@ class UserPickupRequestForm extends FormBase {
                     $guzzle->get("$api_url/patron/$selfCheckApi_key-$patron_barcode/update_hold/" . $hold['holdId'] . "?cancel_time=$cancel_time&cancel_cause=6")->getBody()->getContents();
                 } else {
                     // set the expire date for each selected hold
-                    $updated_hold = $guzzle->get("$api_url/patron/$selfCheckApi_key-$patron_barcode/updated_hold/" . $hold['holdId'] . "?shelf_expire_time=$pickup_date 23:59:59")->getBody()->getContents();
+                    $updated_hold = $guzzle->get("$api_url/patron/$selfCheckApi_key-$patron_barcode/update_hold/" . $hold['holdId'] . "?shelf_expire_time=$pickup_date 23:59:59")->getBody()->getContents();
                     // create arborcat_patron_pickup_request records for each of the selected holds
                     $db->insert('arborcat_patron_pickup_request')
                     ->fields([
