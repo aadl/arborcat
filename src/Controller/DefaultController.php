7f06e2738b8a0e19413552e32ba9433b650465f5
Laurie Kirchmeier <kirchmeierl@aadl.org>
2023-06-26 12:03:02 -0400

    Moved review_form setup inside of the authenticated user block

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -160,9 +160,6 @@ class DefaultController extends ControllerBase {
       $reviews[$k]->username = (isset($review_user) ? $review_user->get('name')->value : 'unknown');
     }
 
-    // set up review form for users
-    $review_form = \Drupal::formBuilder()->getForm('Drupal\arborcat\Form\UserRecordReviewForm', $bib_record->id, $bib_record->title);
-
     // get commuity ratings
     $query = $db->query(
       "SELECT AVG(rating) as average, count(id) as total FROM arborcat_ratings WHERE bib=:bib and rating > 0",
@@ -171,14 +168,19 @@ class DefaultController extends ControllerBase {
     $ratings = $query->fetch();
     $ratings->average = round($ratings->average, 1);
     $ratings->user_rating = '';
-    // retrieve user ratings IF the user is authenticated
+    
+    // retrieve user ratings and set up review form IF the user is authenticated
     if ($user->isAuthenticated()) {
+      // set up review form for users
+      $review_form = \Drupal::formBuilder()->getForm('Drupal\arborcat\Form\UserRecordReviewForm', $bib_record->id, $bib_record->title);
+
       $user_rating = $db->query(
         "SELECT rating FROM arborcat_ratings WHERE bib=:bib AND uid=:uid",
         [':bib' => $bib_record->id, ':uid' => $user->id()]
       )->fetch();
       $ratings->user_rating = $user_rating->rating ?? '';
     }
+    
     // if summer game codes, convert to array so template can loop over
     if (isset($bib_record->gamecodes)) {
       if (\Drupal::moduleHandler()->moduleExists('summergame')) {
